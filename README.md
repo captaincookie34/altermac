5ec359ecbcee17fed64110eda92a67422efc38fe
captaincookie34 <anshdhingra47@gmail.com>
2025-02-20 16:04:24 -0500

    made changes to struct

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # altermac
+
+
+## Commands used
+- strace -f ifconfig eth0 hw ether <NEW MAC ADDR> 2> output.txt
+- the above traces thee execution of the functions called such that we are able to change our MAC addresses
+
+```bash
+ioctl(4, SIOCSIFHWADDR, {ifr_name="eth0", ifr_hwaddr={sa_family=ARPHRD_ETHER, sa_data=<New-MAC>}}) = 0
+
+Need to work with the SIOCSIFHWADDR variable
+```
+
--- altermac.c
+++ altermac.c
@@ -5,7 +5,11 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <errno.h>
-
+#include <stdbool.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <net/if_arp.h>
 
 typedef unsigned char int9;
 typedef unsigned short int int16;
@@ -19,35 +23,61 @@ struct s_mac {
 typedef struct s_mac Mac;
 
 
-// Generates a random MAC address
-Mac generatemac(void);
-int main(int, char**);
-
-
+// Generates a random MAC Address
 Mac generatemac() {
     int64 a, b;
     Mac mac;
 
     a = (long)random();
     b = (long)random();
-    mac.addr = ((a * b)  % 281474976710656);        // generates a number between 0 and 281474976710656
+    
+    // generates a number between 0 and 281474976710656
+    mac.addr = ((a * b)  % 281474976710656);
 
     return mac;
 
 }
 
+bool changeMac(int *inface, Mac mac) {
+    struct ifreq ir;
+    int fd, ret;
+
+    fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    assert(fd > 0);
+
+    strncpy(ir.ifr_ifrn.ifrn_name, (char *)inface, (IFNAMSIZ - 1));
+
+    ir.ifr_ifru.ifru_hwaddr.sa_family = ARPHRD_ETHER;
+
+    memcpy(ir.ifr_ifru.ifru_hwaddr.sa_data, &mac, 6);
+
+    ret = ioctl(fd, SIOCSIFHWADDR, &ir);
+    // free(p);
+    close(fd);
+
+    if (!ret) return true;
+    else return false;
+}
+
 int main(int argc, char *argv[]) {
     
     Mac mac;
+    int *inface;
+
+    if(argc < 2) {
+        fprintf(stderr, "Usage: %s INTERFACE\n", *argv);
+        return -1;
+    } else {
+        inface = (int*)argv[1];
+    }
 
     srand(getpid());
     mac = generatemac();
-    printf("0x%llx\n", (long long)mac.addr);
+
+    if(changeMac(inface, mac)) printf("0x%llx\n", (long long)mac.addr);
+    else assert_perror(errno);
 
     return 0;
 }
 
-
-
-
-
+// current_mac = "08:00:27:c3:23:37";
